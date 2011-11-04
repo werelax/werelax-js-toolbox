418b14b1c6edab1d6c2bef104737293acba20da8
Elías Alonso <eliasagc@gmail.com>
2011-11-04 21:26:26 +0100

    Added Modules.js

---
+++ README.md
@@ -0,0 +1,76 @@
+# WeRelax's JavaScript ToolBox
+
+This is a personal repository for my own convenience. Some place to store some micro-libraries with useful utilities that I have been collecting. Some of them are shameless rip-offs of fragments from other libraries and the rest are mostly reimplementations or a simplified version of someone else's ideas. I have some 100% original material here, but not much and not very good.
+
+### Modules.js
+
+This one is almost a copy-paste from the Module implementation from Spine.js, with a couple of little tweaks. It can be used as a inheritance library, like this:
+
+```javascript
+
+var BaseClass = Wrlx.Class.create({
+    // init is the constructor
+    init: function() {
+        console.log("BaseClass!");
+    },
+    somevalue: "I am a property",
+    somemethod: function (v) {
+        console.log("BaseClass#somemethod!<br>");
+        console.log("somevalue: " + this.somevalue);
+    }
+});
+
+var ChildClass = BaseClass.create({
+    init: function() {
+        console.log("ChildClass!");
+        // Call the superclass constructor
+        this.super('init');
+    },
+    somemethod: function (v) {
+        console.log("ChildClass#somemethod!<br>");
+        // Call superclass#somemethod
+        this.super('somemethod');
+    }
+});
+
+
+var a = new BaseClass;
+a.somemethod();
+
+car b = new ChildClass;
+b.somemethod();
+
+```
+
+But also can be used to build a simple module system. The meaning and the syntax of `Wrlx.Class.include` `Wrlx.Class.extend` are identical to the Spine.js equivalents. A quick example of this:
+
+```javascript
+
+var StaticModule = {
+  say_hi: function() { console.log("I say: Hi there!"); },
+  extended: function() {
+    console.log("StaticModule has been used to extend a class");
+  }
+};
+var InstanceModule = {
+  module_property: "I am from SomeModule",
+  included: function() {
+    console.log("InstanceModule has been included in the instances");
+  }
+};
+
+var MyClass = Wrlx.Class.create({
+  init: function() { console.log(this.module_property); }
+});
+
+// include
+
+MyClass.include(InstanceModule);
+var t = new MyClass();
+
+// extend
+
+MyClass.extend(StaticModule);
+MyClass.say_hi();
+
+```
---
+++ src/modules.js
@@ -0,0 +1,101 @@
+(function (exports) {
+
+  var reserved_keywords = { 'extend':null, 'include':null };
+
+  function Module() {
+    if (typeof this.init === 'function') {
+      this.init.apply(this, arguments);
+    }
+  }
+  Module.include = function (obj) {
+    if (!obj) { throw new Error('include(obj) requires obj'); }
+    for (var key in obj) if (obj.hasOwnProperty(key) && !(key in reserved_keywords)) {
+      this.prototype[key] = obj[key];
+    }
+    if (typeof obj.included === 'function') {
+      obj.included.apply(this);
+    }
+  };
+  Module.extend = function (obj) {
+    if (!obj) { throw new Error('extend(obj) requires obj'); }
+    for (var key in obj) if (obj.hasOwnProperty(key) && !(key in reserved_keywords)) {
+      this[key] == obj[key];
+    }
+    if (typeof obj.extended === 'function') {
+      obj.extended.apply(this);
+    }
+  };
+  Module.proxy = function (fn) {
+    var ctx = this
+    return function() {
+      fn.apply(ctx, arguments);
+    }
+  };
+  Module.create = function (instances, statics) {
+    var _super_ = this;
+    var F = function () {};
+    F.prototype = this.prototype;
+    var child = function () { _super_.apply(this) };
+    child.prototype = new F();
+    child.prototype._super_ = _super_;
+    child.prototype.super = function(name) {
+        var args = Array.prototype.splice.call(arguments, 1);
+        var superfn = _super_.prototype[name];
+        if (typeof superfn === 'function') {
+            return superfn.apply(this, args);
+        }
+    };
+    
+    for (var key in this) if (this.hasOwnProperty(key)) {
+        child[key] = this[key];
+    }
+    if (instances) {
+        child.include(instances);
+    }
+    if (statics) {
+        child.extend(statics);
+    }
+    return child;
+  };
+  Module.prototype = {
+    proxy: function () {
+      var ctx = this;
+      return function() {
+        fn.apply(ctx, arguments);
+      };
+    }
+  };
+
+  exports['Class'] = Module;
+  
+})(Wrlx || (this.Wrlx = {}));
+
+/**
+
+var Jarl = fm.Module.create({
+    init: function() {
+        print("JARL!");
+    },
+    somevalue: "yeah!",
+    othervalue: "jarl",
+    somemethod: function (v) {
+        print("somevalue: " + this.somevalue);
+    }
+});
+
+var Yorl = Jarl.create({
+    init: function() {
+        print("YORL!");
+        this.super('init');
+    },
+    somemethod: function (v) {
+        print("Yorl#somemethod!<br>");
+        this.super('somemethod');
+    }
+});
+
+
+var j = new Yorl();
+j.somemethod();
+
+**/
